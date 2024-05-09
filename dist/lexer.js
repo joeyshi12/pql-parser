83afc983d1c9014adb749323b18306b8004afc78
Joey Shi <joeyshi12@gmail.com>
2024-05-09 07:20:42 -0700

    initial commit

--- dist/lexer.js
+++ dist/lexer.js
@@ -94,13 +94,16 @@ class Lexer {
                     case "USING":
                     case "AS":
                     case "WHERE":
-                    case "AND":
-                    case "OR":
                     case "GROUPBY":
                     case "LIMIT":
                     case "OFFSET":
                         if (!this._currentChar || this._currentChar === " ") {
                             return { type: "KEYWORD", value: identifier.toUpperCase() };
+                        }
+                    case "AND":
+                    case "OR":
+                        if (!this._currentChar || this._currentChar === " ") {
+                            return { type: "LOGICAL_OPERATOR", value: identifier.toUpperCase() };
                         }
                     case "BAR":
                     case "LINE":
